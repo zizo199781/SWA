8ff44a6055c8713325c22265b548d29043aedd30
Abd El Razik <a.elrazik.h@gmail.com>
2019-08-06 09:32:32 +0200

    aa

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/users', function(req, res, next) {
   res.links({ home :'https://calc1611.eu-gb.mybluemix.net/'});
 });
 
